bf259a6ef7188fb76db2613c64c28c39303dd753
Walker Zhao <zhaocework@gmail.com>
2017-09-27 14:54:16 +0800

    增加Retrofit中的Call扩展

---
+++ anke/src/main/java/com/walker/anke/retrofit/CallExtension.kt
@@ -0,0 +1,18 @@
+package com.walker.anke.retrofit
+
+import retrofit2.Call
+
+
+/**
+ * Call 扩展
+ *
+ * Author: Walker
+ * Email: zhaocework@gmail.com
+ * Date: 2017/9/27
+ */
+
+/**
+ * 调用包装器
+ * @param f 数据的处理函数
+ */
+inline fun <T, R> Call<T>.unwrapCall(f: T.() -> R): R = execute().body().f()
