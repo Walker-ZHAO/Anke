b0089267e6f1dc728d84901dc628a7e8a0b0d515
Walker Zhao <zhaocework@gmail.com>
2017-10-11 10:06:23 +0800

    增加Adapter自动更新适配器

---
+++ anke/src/main/java/com/walker/anke/adapter/AutoUpdatableAdapter.kt
@@ -0,0 +1,40 @@
+package com.walker.anke.adapter
+
+import android.support.v7.util.DiffUtil
+import android.support.v7.widget.RecyclerView
+
+/**
+ * 自动更新适配器
+ *
+ * Author: Walker
+ * Email: zhaocework@gmail.com
+ * Date: 2017/10/10
+ */
+interface AutoUpdatableAdapter {
+
+    /**
+     * 使用DiffUtil工具进行Adapter自动更新
+     *
+     * @param oldList   老数据
+     * @param newList   新数据
+     * @param compare   单一项目是否相同的比较
+     *
+     */
+    fun <T> RecyclerView.Adapter<*>.autoUpdate(oldList: List<T>, newList: List<T>, compare: (T, T) -> Boolean) {
+        val diff = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return compare(oldList[oldItemPosition], newList[newItemPosition])
+            }
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return oldList[oldItemPosition] == newList[newItemPosition]
+            }
+
+            override fun getOldListSize() = oldList.size
+
+            override fun getNewListSize() = newList.size
+        })
+
+        diff.dispatchUpdatesTo(this)
+    }
+}
