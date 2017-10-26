888830401eefeac98866286cb69c6b3e15652148
Walker Zhao <zhaocework@gmail.com>
2017-10-26 16:45:38 +0800

    更新gradle版本；更新gson版本；增加context禁用状态栏及导航栏的函数

--- anke/src/main/java/com/walker/anke/framework/ContextExtension.kt
+++ anke/src/main/java/com/walker/anke/framework/ContextExtension.kt
@@ -20,3 +20,49 @@ val Context.processName: String?
                 .filter { it.pid ==  Process.myPid() }
                 .map { it.processName }
                 .firstOrNull()
+
+/**
+ * 禁用导航栏
+ *
+ * Note: 需要使用系统签名
+ */
+fun Context.disableNatigation() {
+    try {
+        val service = getSystemService("statusbar")
+        val statusbarManager = Class.forName("android.app.StatusBarManager")
+        val method = statusbarManager.getMethod("disable", Int::class.javaPrimitiveType)
+        method.invoke(service, DISABLE_NAVIGATION)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+/**
+ * 禁用下拉通知栏
+ *
+ * Note: 需要使用系统签名
+ */
+fun Context.disableNotificationBar() {
+    try {
+        val service = getSystemService("statusbar")
+        val statusbarManager = Class.forName("android.app.StatusBarManager")
+        val method = statusbarManager.getMethod("disable", Int::class.javaPrimitiveType)
+        method.invoke(service, DISABLE_EXPAND)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+val DISABLE_EXPAND = 0x00010000
+val DISABLE_NOTIFICATION_ICONS = 0x00020000
+val DISABLE_NOTIFICATION_ALERTS = 0x00040000
+val DISABLE_NOTIFICATION_TICKER = 0x00080000
+val DISABLE_SYSTEM_INFO = 0x00100000
+val DISABLE_HOME = 0x00200000
+val DISABLE_RECENT = 0x01000000
+val DISABLE_BACK = 0x00400000
+val DISABLE_CLOCK = 0x00800000
+val DISABLE_SEARCH = 0x02000000
+val DISABLE_NONE = 0x00000000
+
+val DISABLE_NAVIGATION = DISABLE_HOME or DISABLE_RECENT
